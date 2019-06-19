fe9eb78570547cd689a49f2e621dbf8eaa3c6b65
jesusamoros <37039972+jesusamoros@users.noreply.github.com>
2019-06-19 09:48:04 +0200

    Update launch_now.sh

--- launch_now.sh
+++ launch_now.sh
@@ -1,15 +1,30 @@
+#clonamos el repo 
+git clone repositori.git
 #construimos el docker con el docker file
 docker build -t nombre_cont_back -f ./Dockerfile.dev .
 
 #Creamos una network
 docker network create mynet
 
-#Eliminamos por si ya existe el contenedor.
+#Eliminamos 
 docker rm name_contenedor -f
 
-#Creamos el contenedor por ejemplo de mongo
+#Arrancamos el contenedor por ejemplo de mongo
 docker run -d --name mongo -v $PWD/volumes/mongo:/etc/mongo --net dgn -p 27017:27017 mongo
 
-#Eliminamos por si ya existe el  contenedor del back.
+#Eliminamos 
 docker rm  nombre_cont_back -f
 docker run -v $PWD:/repo -d --name nombre_cont_back --net mynet -p 3000:3000 nombre_cont_back
+
+#Ahora el FRONT
+#clonamos el repo 
+git clone front.repositorio.git
+
+##construimos el docker con el docker file
+docker build -t front-container -f ./Dockerfile .
+
+#Eliminamos 
+docker rm front-container -f
+
+#Arrancamos el contenedor por ejemplo de mongo
+docker run -d --name front-container-v $PWD:/repo --net proxy front-container
